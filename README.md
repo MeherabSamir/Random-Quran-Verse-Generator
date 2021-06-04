fdf811ac38418af6913547e2d7f59c8bef08d90d
MeherabSamir <meherabsamir.me@gmail.com>
2021-06-04 18:07:43 +0600

    Random Quran Verse Generator

---
+++ README.md
@@ -0,0 +1,53 @@
+# Random Quran Verse Generator
+Random Quran Verse Generator By [Meherab Samir][Github Link]
+
+Tecnologies Used
+- HTML
+- CSS
+- JAVASCRIPT 
+
+Special Credit
+- [Quran.com][Quran.com]
+- [Alquran.cloud][Al-Quran.cloud]
+
+<br>
+
+![Github][Github-sheild]
+![][Code-sheild]
+
+### Preview Image
+<br>
+
+[![Preview Image](assets/img/preview.png)][Live Demo]
+
+
+Contact me - [meherabsamir.me@gmail.com](mailto:meherabsamir.me@gmail.com)
+
+[![Twitter][Twitter-shield]][Twitter]
+[![Facebook][Facebook-sheild]][Facebook]
+[![Instragram][Instagram-sheild]][Instagram]
+
+<!-- Links -->
+[Github Link]: https://github.com/MeherabSamir
+
+[Live Demo]: https://randomquranverse.netlify.app/ "Click to see live demo."
+
+[Twitter]:https://twitter.com/MeherabSamir
+
+[Twitter-shield]: https://img.shields.io/twitter/follow/MeherabSamir?label=MeherabSamir&style=social
+
+[Instagram]: https://instragram.com/meherab_samir
+
+[Instagram-sheild]: https://img.shields.io/badge/Instragram-Meherab%20Samir-%233690ff?logo=instagram&style=social
+
+[Facebook]:https://www.facebook.com/mohammadsamir.meherab/
+
+[Facebook-sheild]: https://img.shields.io/badge/Facebook-Meherab%20Samir-%233690ff?logo=facebook&style=social
+
+[Github-sheild]:https://img.shields.io/github/watchers/MeherabSamir/Random-Quran-Verse-Generator?style=social
+
+[Code-sheild]:https://img.shields.io/tokei/lines/github/MeherabSamir/Random-Quran-Verse-Generator?color=%233690ff&logo=github?logo=appveyor
+
+[Quran.com]: https://quran.com/
+
+[Al-Quran.cloud]: https://alquran.cloud/
---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Selecting DOM Elements
+const arabicAyah = document.querySelector('.arabic-ayah')
+const banglaAyah = document.querySelector('.bangla-ayah')
+const suraNumber = document.querySelector('sura-number')
+const tweetShare = document.querySelector('.twitter-share')
+const audio = document.querySelector('audio')
+const btnPlay = document.querySelector('.btn-play')
+const generateBtn = document.querySelector('.btn-generate')
+const suraName = document.querySelector('.sura-name')
+const ayahNum = document.querySelector('.ayah-number')
+const suraInfo = document.querySelector('.sura-info')
+
+
+// Random Number
+function generateRandomNum(){
+    return Math.floor(Math.random() * 6236)
+}
+const randomNum = generateRandomNum()
+
+
+// Fetching API
+const arabicApi = `http://api.alquran.cloud/v1/ayah/${randomNum}/ar.alafasy`
+const banglaApi = `http://api.alquran.cloud/v1/ayah/${randomNum}/bn.bengali`
+
+const arabic = fetch(arabicApi)
+                .then(blob => blob.json())
+                .then(data => {
+                    arabicAyah.textContent = `${data.data.text}`
+                    suraName.textContent = `${data.data.surah.englishName}`
+                    ayahNum.textContent = `${data.data.numberInSurah}`
+                    audio.src = data.data.audio
+                    console.log(data)
+                })
+
+const bangla = fetch(banglaApi)
+                .then(blob => blob.json())
+                .then(data => {
+                    banglaAyah.textContent = `${data.data.text}`
+                })
+
+
+// Event Listener
+btnPlay.addEventListener('click', () =>{
+    audio.play()
+})
+generateBtn.addEventListener('click',()=>{
+    location.reload()
+})
+
+
+// Tweet
+function tweet(){
+    tweetShare.setAttribute('href',`https://twitter.com/intent/tweet/?text=${banglaAyah.innerText} আল-কুরআন।`)
+}
+
+setInterval(tweet,1000)
